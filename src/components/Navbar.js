c6b508c2541f80dd116624cac5ea1d0cd1b1c433
daveholmes88 <github email address>
2020-10-15 15:05:57 -0500

    Navbar start

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Nav, Navbar, Button, Container } from 'react-bootstrap';
+import { Nav, Navbar, Button, Container, NavDropdown } from 'react-bootstrap';
 
 export default function NavBar() {
 
@@ -10,6 +10,13 @@ export default function NavBar() {
                     <Nav.Item>
                         <h3>Bruce Holmes</h3>
                     </Nav.Item>
+                </Nav>
+                <Nav className="mr-sm-2">
+                    <NavDropdown alignRight id='nav-dropdown' title={<span><i className="navbar-toggler-icon"></i></span>}>
+                        <NavDropdown.Item href="/example-app/section-a/">Section A</NavDropdown.Item>
+                        <NavDropdown.Divider />
+                        <NavDropdown.Item href="/logout">Logout</NavDropdown.Item>
+                    </NavDropdown>
                 </Nav>
             </Container>
         </Navbar>
