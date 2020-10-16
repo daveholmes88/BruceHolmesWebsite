815d3456308def444ff685c33773c4a94a2f1221
daveholmes88 <github email address>
2020-10-16 13:30:20 -0500

    carousel start

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,21 +1,33 @@
 import React from 'react';
-import { Nav, Navbar, Button, Container, NavDropdown } from 'react-bootstrap';
+import { Nav, Navbar, Container, NavDropdown } from 'react-bootstrap';
 
 export default function NavBar() {
 
     return (
-        <Navbar sticky='top' >
+        <Navbar sticky='top' bg="primary" variant="dark">
             <Container>
                 <Nav>
                     <Nav.Item>
-                        <h3>Bruce Holmes</h3>
+                        <Nav.Link href="/"><h3>Bruce Holmes</h3></Nav.Link>
                     </Nav.Item>
                 </Nav>
                 <Nav className="mr-sm-2">
                     <NavDropdown alignRight id='nav-dropdown' title={<span><i className="navbar-toggler-icon"></i></span>}>
-                        <NavDropdown.Item href="/example-app/section-a/">Section A</NavDropdown.Item>
+                        <NavDropdown.Item href="/music">Music</NavDropdown.Item>
                         <NavDropdown.Divider />
-                        <NavDropdown.Item href="/logout">Logout</NavDropdown.Item>
+                        <NavDropdown.Item href="/feldenkrais">Feldenkrais</NavDropdown.Item>
+                        <NavDropdown.Divider />
+                        <NavDropdown.Item href="/dance">Dance</NavDropdown.Item>
+                        <NavDropdown.Divider />
+                        <NavDropdown.Item href="/Triathlon">Triathlon</NavDropdown.Item>
+                        <NavDropdown.Divider />
+                        <NavDropdown.Item href="/sciencefiction">Science Fiction</NavDropdown.Item>
+                        <NavDropdown.Divider />
+                        <NavDropdown.Item href="/twocents">Two Cents</NavDropdown.Item>
+                        <NavDropdown.Divider />
+                        <NavDropdown.Item href="/contact">Contact</NavDropdown.Item>
+                        <NavDropdown.Divider />
+                        <NavDropdown.Item href="/shopping">Shopping</NavDropdown.Item>
                     </NavDropdown>
                 </Nav>
             </Container>
