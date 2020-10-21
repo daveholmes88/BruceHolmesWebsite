3a279bea4bf31f6655f983ae15173613fbb6c4f1
daveholmes88 <github email address>
2020-10-21 13:13:54 -0500

    Feldenkrais start

--- src/components/Feldenkrais.js
+++ src/components/Feldenkrais.js
@@ -1,19 +1,58 @@
 import React from 'react'
-import { Container, Row, Col } from 'react-bootstrap'
+import { Container, Row, Col, Card, Button } from 'react-bootstrap'
 
 export default function Feldenkrais() {
 
     return (
         <Container>
             <Row>
+                <Col sm={3}>
+                    <Card border='dark'>
+                        <img
+                            className="feldenkrais"
+                            src='Feldenkrais.jpg'
+                        />
+                        <h3>The Feldenkrais Method</h3>
+                        <br></br>
+                        <Button>
+                            History
+                        </Button>
+                        <br></br>
+                        <Button>
+                            Index
+                        </Button>
+                        <br></br>
+                        <Button>
+                            Press Quotes
+                        </Button>
+                        <br></br>
+                        <Button>
+                            User Quotes
+                        </Button>
+                        <br></br>
+                        <Button>
+                            Free Lesson
+                        </Button>
+                        <br></br>
+                        <Button>
+                            Runner's World Article
+                        </Button>
+                        <br></br>
+                        <Button>
+                            Buy Now
+                        </Button>
+                    </Card>
+                </Col>
                 <Col>
-
+                    <h1>The Feldenkrais Method</h1>
+                    <h3>The Feldenkrais Lessons: Awareness Through Movement</h3>
+                    <br></br>
+                    <h3>A 48 lesson home study course for $48</h3>
+                    <Button>
+                        Buy Now
+                    </Button>
                 </Col>
             </Row>
-            <h1>The Feldenkrais Method</h1>
-            <h3>The Feldenkrais Lessons: Awareness Through Movement</h3>
-            <br></br>
-            <h3>A 48 lesson home study course for $48</h3>
         </Container>
     )
 }
